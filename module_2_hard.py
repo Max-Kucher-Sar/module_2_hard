8b033df276a3af8abcbe5537a7b1f23147035b4b
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-08-21 19:55:47 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,9 +1,10 @@
-
-for k in range(3, 21):
-    str_ = ''
+password = int(input('Введите число: '))
+str_ = []
+for k in range(3, password + 1):
     for i in range(1, k):
         for j in range(i + 1, k):
             if k % (i + j) == 0:
-                str_ = f'{i}{j}'
-                print(k, str_)
+                str_.append(str(i) + str(j))
 
+print('Число: ', password)
+print('Пароль: ', *str_, sep='')
